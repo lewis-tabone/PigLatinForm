fbfeadec3e90adc314b0d211a8807388680f131f
Lewis <Lewis@DESKTOP-FAOVK91>
2021-09-06 21:17:46 +1000

    Add project files.

---
+++ PigLatinForm/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PigLatinForm
+{
+    partial class Form1
+    {
+       
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+       
+
+        
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Translate = new System.Windows.Forms.Button();
+            this.InputBox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Translate
+            // 
+            this.Translate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Translate.Location = new System.Drawing.Point(252, 185);
+            this.Translate.Name = "Translate";
+            this.Translate.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Translate.Size = new System.Drawing.Size(347, 39);
+            this.Translate.TabIndex = 0;
+            this.Translate.Text = "Translate";
+            this.Translate.UseVisualStyleBackColor = true;
+            this.Translate.Click += new System.EventHandler(this.Translate_Click_1);
+            // 
+            // InputBox
+            // 
+            this.InputBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputBox.Location = new System.Drawing.Point(13, 12);
+            this.InputBox.Multiline = true;
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(775, 140);
+            this.InputBox.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(438, 258);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(350, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OutputBox.Location = new System.Drawing.Point(-1, 258);
+            this.OutputBox.Multiline = true;
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(433, 180);
+            this.OutputBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.Translate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OutputBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.Translate);
+            this.Name = "Form1";
+            this.Text = "Pig Latin Translator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Translate;
+        private System.Windows.Forms.TextBox InputBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox OutputBox;
+    }
+}
+
---
+++ PigLatinForm/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PigLatinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Translate_Click_1(object sender, EventArgs e)
+        {
+            string sentence = InputBox.Text;
+            //TranslateToPigLatin(sentence);
+            OutputBox.Text = TranslateToPigLatin(sentence);
+        }
+        /*Console.WriteLine("Please enter a sentence to convert to Pig Latin");
+            string sentence = InputBox.text;
+        string pigLatin = TranslateToPigLatin(sentence);    //Runs function over user input
+        Console.WriteLine(pigLatin);*/
+        public string TranslateToPigLatin(string sentence)
+        {
+            //Console.WriteLine("Please enter a sentence to convert to Pig Latin");
+
+            //string pigLatin = TranslateToPigLatin(sentence);    //Runs function over user input
+            //Console.WriteLine(pigLatin);
+
+            string[] vowels = { "a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y" }; //string array of vowels, (including Y) for PigLatin rules.
+
+
+            List<string> pigWords = new List<string>(); //Instantiate new pigWords List
+
+            // Break sentence down into words
+            foreach (string word in sentence.Split(' '))
+            {
+
+
+                int firstVowelPosition = 1; //Instantiate and set with value.
+                string firstLetter = word.Substring(0, 1); //assign the first letter of the originally handled word
+                bool isFirstLetterCaps = char.IsUpper(char.Parse(firstLetter)); //check if first letter is capitalised
+
+                for (int i = 0; i < word.Length; i++) //Iterate over each char in each word.
+                {
+                    string v = word[i].ToString(); //cast lambda position of word to string
+                    bool isLetterAVowel = vowels.Contains(v); //check if letter is a vowel (including y)
+                    if (isLetterAVowel)
+                    {
+                        firstVowelPosition = i; //assign position of vowel to variable
+                        break;
+                    }
+                }
+
+                string beforeVowel = word.Substring(0, firstVowelPosition); //assign all consanants before first vowel to variable
+                string restOfWord = word.Substring(firstVowelPosition, word.Length - firstVowelPosition); //assign all remaining chars to variable
+                string pigWord = null; //Instantiate variable to null
+
+
+                // Rule 1 - Vowel in first position, add "way" to end
+                if (firstVowelPosition == 0)
+                {
+                    pigWord = word.ToLower() + "way"; //decapitalise final word
+                }
+
+                // Rule 2 - Vowel in second or greater position,
+                //          move consonants before vowel to end
+                //          and add "ay" to end
+                else if (firstVowelPosition > 0)
+                {
+                    pigWord = restOfWord.ToLower() + beforeVowel.ToLower() + "ay"; //decapitalise final word
+                }
+
+                if (isFirstLetterCaps)
+                {
+                    // change pigWords first letter to upper case
+                    string completedWord = pigWord.Substring(0, 1).ToUpper() + pigWord.Substring(1);
+                    pigWords.Add(completedWord);
+                }
+                else
+                {
+                    pigWords.Add(pigWord); //if no capitalised first letter was detected, simply add the word as lowercase
+                }
+            }
+            
+            return string.Join(" ", pigWords); //add complete pigwords to string and return it to user
+
+        }
+    }
+}
